f7ad7d2137ff5d467e323e93223453a695612b31
icaro <icaro@icaro-Ideapad-Z460>
2013-08-31 13:24:43 -0300

    Item Update

--- item.java
+++ item.java
@@ -1,4 +1,6 @@
-public abstract class Item { 
+import java.util.Calendar;
+
+public abstract class Item implements Pesquisavel { 
   
     private String nome; 
     private String historicoUsuario;
@@ -8,13 +10,56 @@ public abstract class Item {
     private Calendar date;
   
     
-   public Item(String nome, Usuario doador){
+   public Item(String nome, Usuario usuarioAtual){
 	   
 	   this.nome = nome;
-	   this.doador = doador;
+	   this.usuarioAtual = usuarioAtual;
+	   numDeEmprestimos = 1;
+	   
    }
    
-    
+public double Favoritar (int valor){
+	   
+   double novovalor = (estrelas + valor) / numDeEmprestimos;
    
+   		return novovalor;
+   }
+
+public String getHistoricoUsuario() {
+	return historicoUsuario;
+}
+
+public void setHistoricoUsuario(String historicoUsuario) {
+	this.historicoUsuario = historicoUsuario;
+}
+
+public Usuario getUsuarioAtual() {
+	return usuarioAtual;
+}
+
+public void setUsuarioAtual(Usuario usuarioAtual) {
+	this.usuarioAtual = usuarioAtual;
+}
+
+public double getEstrelas() {
+	return estrelas;
+}
+
+public void setEstrelas(double estrelas) {
+	this.estrelas = estrelas;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public int getNumDeEmprestimos() {
+	return numDeEmprestimos;
+}
+
+public Calendar getDate() {
+	return date;
+}
+    
    
 } 
